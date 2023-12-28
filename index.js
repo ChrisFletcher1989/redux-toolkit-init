4c9966f299f4d159e0e5e931af0577ce2329555f
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-12-28 11:56:37 +0900

    added iceCream state

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //import the store and actions
 const store = require("./app/store");
 const cakeActions = require("./app/features/cake/cakeSlice").cakeActions;
+const iceCreamActions =
+  require("./app/features/icecream/iceCreamSlice").iceCreamActions;
 
 //subscribe to listen for changes
 const unsubscribe = store.subscribe(() => {
@@ -11,5 +13,8 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.restocked(2));
 //finally unsubscribe
 unsubscribe();
