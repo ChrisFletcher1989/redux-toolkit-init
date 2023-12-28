46ea25c638364f30048955e9a1744559082f70c7
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-12-28 16:24:21 +0900

    added async thunk users

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const store = require("./app/store");
 const cakeActions = require("./app/features/cake/cakeSlice").cakeActions;
 const iceCreamActions =
   require("./app/features/icecream/iceCreamSlice").iceCreamActions;
+const fetchUsers = require("./app/features/user/userSlice").fetchUsers;
 
 //subscribe to listen for changes
 const unsubscribe = store.subscribe(() => {
   console.log("Updated State", store.getState());
 });
-//dispatch actionbs
+//dispatch actions
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
@@ -16,5 +17,6 @@ store.dispatch(cakeActions.restocked(3));
 store.dispatch(iceCreamActions.ordered());
 store.dispatch(iceCreamActions.ordered());
 store.dispatch(iceCreamActions.restocked(2));
+store.dispatch(fetchUsers());
 //finally unsubscribe
-unsubscribe();
+// unsubscribe();
